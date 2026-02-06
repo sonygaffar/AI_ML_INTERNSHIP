da543a0608244c64b8e8818b47102d4abe7f337c
sonygaffar <sonybushrasonybushra@gmail.com>
2026-02-06 13:10:28 +0530

    day 5 task 2

---
+++ day_5/main.py
@@ -0,0 +1,8 @@
+import calculator
+a =int(input("Enter first number:"))
+b =int(input("Enter secound number:"))
+print("Addition:", calculator.add(a,b))
+print("Subtraction:", calculator.sub(a,b))
+print("Multiplication:", calculator.mul(a,b))
+print("Division:", calculator.div(a,b))
+
